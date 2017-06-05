6df22f8dfd9c7ab049b71ef8694b3331bdf08362
youths <552099787@qq.com>
2017-06-05 16:15:41 +0800

    test版

--- main/main.js
+++ main/main.js
@@ -1,4 +1,64 @@
-module.exports = function main() {
+module.exports = function main(inputs) {
+	var result=[];
+	for (var index = 0; index < inputs.length ; index++)
+	{
+		result.push( returnNumber(inputs.charAt(index)));
+	}
+	printLedNumber(result);
     console.log("Debug Info");
-    return 'Hello World!';
+    //return 'Hello World!';
+};
+
+function printLedNumber(result){
+	for (var row = 0;row < result.length ; row++)
+	{
+		console.log(result[index]);
+	}
+};
+
+function returnNumber(inputs){
+	var arrayNumber = {{".",".","."},{".",".","."},{".",".","."}};
+	switch (inputs)
+	{
+	case "0":
+		arrayNumber[1][0]=arrayNumber[1][2]=arrayNumber[2][0]=arrayNumber[2][1]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "1":
+		arrayNumber[1][2]=arrayNumber[2][2]="|";
+		return arrayNumber ;
+	case "2":
+		arrayNumber[1][2]=arrayNumber[2][0]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "3":
+		arrayNumber[1][2]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "4":
+		arrayNumber[1][0]=arrayNumber[1][2]=arrayNumber[2][2]="|";
+		arrayNumber[1][1]="_";
+		return arrayNumber ;
+	case "5":
+		arrayNumber[1][0]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "6":
+		arrayNumber[1][0]=arrayNumber[2][0]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "7":
+		arrayNumber[1][2]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]="_";
+		return arrayNumber ;
+	case "8":
+		arrayNumber[1][0]=arrayNumber[1][2]=arrayNumber[2][0]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]=arrayNumber[2][1]="_";
+		return arrayNumber ;
+	case "9":
+		arrayNumber[1][0]=arrayNumber[1][2]=arrayNumber[2][2]="|";
+		arrayNumber[0][1]=arrayNumber[1][1]="_";
+		return arrayNumber ;
+	}
+	
 };
