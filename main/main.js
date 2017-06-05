ba70618b5407e5c5013ac927974cdfbacfd759c8
youths <552099787@qq.com>
2017-06-05 21:37:36 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -20,8 +20,10 @@ function mergeResult(result){
 		{
 			for (var j = 0; j < result[0][i].length ; j++)
 			{
-				if((i !=0 )&&( r != 0)&&( j != 0))
+				if((i ==0 )&&( r == 0)&&( j == 0))
 				{
+					continue;
+				}else{
 					str += result[r][i][j];
 				}
 			}
